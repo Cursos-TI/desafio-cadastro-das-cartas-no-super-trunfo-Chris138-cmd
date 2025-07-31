99381c7a1d25e1df5e3dfdfcb4c182e3560d9f81
Chris138-cmd <christopher.douglas97@gmail.com>
2025-07-31 09:53:31 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,12 +7,19 @@
 //Teste larissa
 
 int main() {
-    char Estado;
+     char Estado;
     char Codigo[4];
     char Cidade[50];
     int Populacao;
     float Pib;
     float Area;
+
+    char Estado2;
+    char Codigo2[4];
+    char Cidade2[50];
+    int Populacao2;
+    float Pib2;
+    float Area2;
 
     printf(" Informe os Dados da Carta 01;\n");
     
@@ -34,14 +41,45 @@ int main() {
     printf("Informe a Area:");
     scanf(" %f", &Area);
     
+printf(" Informe os Dados da Carta 02;\n");
+    
+    printf("Estado (Entre A e H):");
+    scanf(" %c", &Estado2);
+
+    printf("Informe um Codigo para a Carta:");
+    scanf(" %s", Codigo2);
+
+    printf("Informe a Cidade:");
+    scanf(" %s", Cidade2);
+
+    printf("Informe a População:");
+    scanf(" %d", &Populacao2);
+        
+    printf("Informe o Pib:");
+    scanf(" %f", &Pib2);
+
+    printf("Informe a Area:");
+    scanf(" %f", &Area2);
+
     printf("\n------------------------------\n");
     printf("-------- Carta 01 -----------\n");
     printf("Estado: %c\n", Estado);
     printf("Código: %s\n", Codigo);
     printf("Cidade: %s\n", Cidade);
     printf("População: %d habitantes\n", Populacao);
-    printf("SPIB: %.2f bilhões de reais\n", Pib);
+    printf("PIB: %.2f\n", Pib);
     printf("Área: %.2f km²\n", Area);
+    printf("------------------------------\n");
+
+
+    printf("\n------------------------------\n");
+    printf("-------- Carta 02 -----------\n");
+    printf("Estado: %c\n", Estado2);
+    printf("Código: %s\n", Codigo2);
+    printf("Cidade: %s\n", Cidade2);
+    printf("População: %d habitantes\n", Populacao2);
+    printf("PIB: %.2f\n", Pib2);
+    printf("Área: %.2f km²\n", Area2);
     printf("------------------------------\n");
 
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
