7f96a0d01828cf40241ef7b95d6f8d625023861f
Chris138-cmd <christopher.douglas97@gmail.com>
2025-07-31 08:32:22 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,33 @@
 //Teste larissa
 
 int main() {
+    char Estado;
+    char Codigo[4];
+    char Cidade[50];
+    int População;
+    float Pib;
+    float Area;
+
+    ("printf Informe os Dados da Carta 01;\n")
+    
+    ("printf Estado (A-H):\n")
+    ("scanf %c, &Estado");
+
+    ("printf Codigo da Carta:\n");
+    ("scanf %s, &Codigo")
+
+    ("printf Informe a População:\n")
+    ("scanf %d, &População")
+        
+    ("printf Informe o Pib")
+    ("scanf, %f &Pib")
+
+    ("printf Informe a Area")
+    ("scanf, %f &Area")   
+    
+
+    
+    
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
